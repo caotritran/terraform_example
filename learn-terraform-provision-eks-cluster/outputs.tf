23385858976855b79c2a4f87d10e7b0aa517ed25
tritran <tri@emobi.biz>
2023-09-25 10:14:23 +0700

    update new tf create eks

---
+++ learn-terraform-provision-eks-cluster/outputs.tf
@@ -0,0 +1,66 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+output "cluster_endpoint" {
+  description = "Endpoint for EKS control plane"
+  value       = module.eks.cluster_endpoint
+}
+
+output "cluster_security_group_id" {
+  description = "Security group ids attached to the cluster control plane"
+  value       = module.eks.cluster_security_group_id
+}
+
+output "region" {
+  description = "AWS region"
+  value       = var.region
+}
+
+output "cluster_name" {
+  description = "Kubernetes Cluster Name"
+  value       = module.eks.cluster_name
+}
+
+output "cluster_certificate_authority_data" {
+  description = "Kubernetes Cluster certificate"
+  value       = module.eks.cluster_certificate_authority_data
+}
+
+locals {
+    kubeconfig = <<KUBECONFIG
+
+
+apiVersion: v1
+clusters:
+- cluster:
+    server: ${module.eks.cluster_endpoint}
+    certificate-authority-data: ${module.eks.cluster_certificate_authority_data}
+  name: kubernetes
+contexts:
+- context:
+    cluster: kubernetes
+    user: aws
+  name: aws
+current-context: aws
+kind: Config
+preferences: {}
+users:
+- name: aws
+  user:
+    exec:
+      apiVersion: client.authentication.k8s.io/v1alpha1
+      command: aws
+      args:
+        - eks
+        - get-token
+        - --cluster-name
+        - ${module.eks.cluster_name}
+        - --region
+        - ${var.region}
+      env:
+        - name: AWS_DEFAULT_REGION
+          value: ${var.region}
+        - name: AWS_PROFILE
+          value: terraform
+KUBECONFIG
+}
